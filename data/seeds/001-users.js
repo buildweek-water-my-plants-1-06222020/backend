0330f7b33b5a14281978f1178651d0603087fa77
Brian Russell <bruss@brians-mbp.lan>
2020-06-22 17:15:42 -0400

    Created Database file for production

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex.raw('TRUNCATE TABLE users CASCADE')
+  return knex('plants').truncate()
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
